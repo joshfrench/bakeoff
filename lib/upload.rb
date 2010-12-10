e0fb5638f472e7fcc8723ee8e6599fa8a106306d
Josh French <josh@digitalpulp.com>
2010-12-10 14:36:46 -0500

    Add image processing

--- lib/upload.rb
+++ lib/upload.rb
@@ -1,4 +1,11 @@
+require 'carrierwave'
+require 'carrierwave/processing/rmagick'
+
 class Upload < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+
+  process :resize_to_fill => [80, 80]
+
   def store_dir
     "#{model.id}"
   end
